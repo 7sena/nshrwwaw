2c92c4b9c6137c8525c502a030d3bddc0e1336a0
7sena <40254036+7sena@users.noreply.github.com>
2018-09-11 23:38:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,45 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-Client.on("guildMemberRemove", member => {
-  member.send(`
-**اهلن فيك في سيرفرنا  تشرفنا 
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-
-➤ Tier: I
-➤ Invites: 10 
-➤ Credit: 10K :yen: 
-
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-
-➤ Tier: II
-➤ Invites: 25 
-➤ Credit: 30K :yen: 
-
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-
-➤ Tier: III
-➤ Invites: 50 
-➤ Credit: 70K :yen: 
-
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-
-➤ Tier: IV
-➤ Invites: 75
-➤ Credit: 100K :yen: 
-
-▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
-
-➤ Tier: V
-➤ Invites: 100 
-➤ Credit: 150K :yen: 
-https://discord.gg/SGcdsS
-**
-[${member}]`);
-
-  });
- 
-
   Client.on('guildMemberAdd', member => {
   member.send(`
 **اهلن فيك في سيرفرنا  تشرفنا 

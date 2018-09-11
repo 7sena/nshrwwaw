65a5f9303df44048e41ea9cabe5339fa09ee93f5
7sena <40254036+7sena@users.noreply.github.com>
2018-09-11 23:31:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,82 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-const adminprefix = "$";
-const devs = ['414477438869831682'];
-client.on('message', message => {
-  var argresult = message.content.split(` `).slice(1).join(' ');
-    if (!devs.includes(message.author.id)) return;
+Client.on("guildMemberRemove", member => {
+  member.send(`
+**اهلن فيك في سيرفرنا  تشرفنا 
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
 
-if (message.content.startsWith(adminprefix + 'sp')) {
-  client.user.setGame(argresult);
-    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
-} else
-  if (message.content.startsWith(adminprefix + 'sn')) {
-client.user.setUsername(argresult).then
-    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
-return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
-} else
-  if (message.content.startsWith(adminprefix + 'sa')) {
-client.user.setAvatar(argresult);
-  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
-      } else
-if (message.content.startsWith(adminprefix + 'st')) {
-  client.user.setGame(argresult, "https://www.twitch.tv/idk");
-    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
-}
+➤ Tier: I
+➤ Invites: 10 
+➤ Credit: 10K :yen: 
 
-});
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: II
+➤ Invites: 25 
+➤ Credit: 30K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: III
+➤ Invites: 50 
+➤ Credit: 70K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: IV
+➤ Invites: 75
+➤ Credit: 100K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: V
+➤ Invites: 100 
+➤ Credit: 150K :yen: 
+https://discord.gg/SGcdsS
+**
+[${member}]`);
+
+  });
+ 
+
+  Client.on('guildMemberAdd', member => {
+  member.send(`
+**اهلن فيك في سيرفرنا  تشرفنا 
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: I
+➤ Invites: 10 
+➤ Credit: 10K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: II
+➤ Invites: 25 
+➤ Credit: 30K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: III
+➤ Invites: 50 
+➤ Credit: 70K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: IV
+➤ Invites: 75
+➤ Credit: 100K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: V
+➤ Invites: 100 
+➤ Credit: 150K :yen: 
+https://discord.gg/SGcdsS
+**
+[${member}]`);
+
+  });
 
 
-client.login("NDE0NDc3NDM4ODY5ODMxNjgy.Dms66Q.1t5__jNPNSyY42NeLC02KK_Zc4M")
+client.login("NDc5MzI4NDUwNTc0ODExMTU4.DnmdGw.qU7u5MTfa-FrI0h8vwqrag-R0kU")

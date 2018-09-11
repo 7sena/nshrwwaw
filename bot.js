0a1474b62fb81ac208802e2fd6159ed858b403d8
7sena <40254036+7sena@users.noreply.github.com>
2018-09-11 23:54:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -63,6 +63,42 @@ https://discord.gg/SGcdsS
 
   });
  
+  client.on('guildMemberAdd', member => {
+  member.send(`
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: I
+➤ Invites: 10 
+➤ Credit: 10K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: II
+➤ Invites: 25 
+➤ Credit: 30K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: III
+➤ Invites: 50 
+➤ Credit: 70K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: IV
+➤ Invites: 75
+➤ Credit: 100K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: V
+➤ Invites: 100 
+➤ Credit: 150K :yen: 
+https://discord.gg/SGcdsS
+[${member}]`);
+
+
+  });
 
 
 client.login("NDc5MzI4NDUwNTc0ODExMTU4.DnmdGw.qU7u5MTfa-FrI0h8vwqrag-R0kU")

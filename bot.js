8b0f60e2ea012467c2785923b3f57886f814a976
7sena <40254036+7sena@users.noreply.github.com>
2018-09-26 21:40:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -108,8 +108,116 @@ https://discord.gg/agSQ7F
 
   });
 
-client.login("NDkyNTk4NjgxNzAzMjg0NzQz.Dokw7g.mBOkoNZyyLpJUJ8UqWzWlKpuSZw")
+client.login("NDkyNTk4NjgxNzAzMjg0NzQz.Dokw7g.mBOkoNZyyLpJUJ8UqWzWlKpuSZw");
 
 
 const client2 = new Discord.Client();
-client2.login("NDc0MTQ5NjIyMjc3ODY1NDc0.Do1hlw.r4kCFpCygTzfacg8hN8ZZSFIBXg")
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log(`channels! [ " ${client.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+});
+
+
+
+client.on("guildMemberRemove", member => {
+  member.send(`**
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: I
+➤ Invites: 10 
+➤ Credit: 10K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: II
+➤ Invites: 25 
+➤ Credit: 30K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: III
+➤ Invites: 50 
+➤ Credit: 60K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: IV
+➤ Invites: 75
+➤ Credit: 100K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: V
+➤ Invites: 100 
+➤ Credit: 150K :yen: 
+
+
+https://discord.gg/agSQ7F
+@everyone || @here 
+**
+[${member}]`);
+
+
+  });
+
+client.on("guildMemberAdd", member => {
+  member.send(`**
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: I
+➤ Invites: 10 
+➤ Credit: 10K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: II
+➤ Invites: 25 
+➤ Credit: 30K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: III
+➤ Invites: 50 
+➤ Credit: 60K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: IV
+➤ Invites: 75
+➤ Credit: 100K :yen: 
+
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+
+➤ Tier: V
+➤ Invites: 100 
+➤ Credit: 150K :yen: 
+
+https://discord.gg/agSQ7F
+@everyone || @here 
+**
+[${member}]`);
+
+
+  });
+
+client2.login("NDc0MTQ5NjIyMjc3ODY1NDc0.Do1hlw.r4kCFpCygTzfacg8hN8ZZSFIBXg");

4ac756a012126231fbe118b482a751c6631af053
7sena <40254036+7sena@users.noreply.github.com>
2018-09-27 20:38:33 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -221,3 +221,66 @@ https://discord.gg/agSQ7F
   });
 
 client2.login("NDc0MTQ5NjIyMjc3ODY1NDc0.Do1hlw.r4kCFpCygTzfacg8hN8ZZSFIBXg");
+
+
+const client3 = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client3.user.tag}!`);
+  console.log('')
+  console.log('')
+  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
+  console.log(`[Start] ${new Date()}`);
+  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════════════════════════════]╗');
+  console.log(`Logged in as * [ " ${client3.user.username} " ]`);
+  console.log('')
+  console.log('Informations :')
+  console.log('')
+  console.log(`servers! [ " ${client3.guilds.size} " ]`);
+  console.log(`Users! [ " ${client3.users.size} " ]`);
+  console.log(`channels! [ " ${client3.channels.size} " ]`);
+  console.log('╚[════════════════════════════════════]╝')
+  console.log('')
+  console.log('╔[════════════]╗')
+  console.log(' Bot Is Online')
+  console.log('╚[════════════]╝')
+  console.log('')
+});
+
+
+
+
+
+client3.on("guildMemberRemove", member => {
+  member.send(`**
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+اكبر ستور راح يساعدك اتمنه دخوله 
+بشترفنا كثير اتمنه دخولك 
+اتمنه تنور السيرفر بنسوي اكبر سيرفر انشاء الله 
+و في فقرة نشر سيرفرات كل اسبوع ادخل
+  <https://discord.gg/vF6CG>
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+** 
+[${member}]`);
+
+
+  });
+
+client3.on("guildMemberAdd", member => {
+  member.send(`**
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+اكبر ستور راح يساعدك اتمنه دخوله 
+بشترفنا كثير اتمنه دخولك 
+اتمنه تنور السيرفر بنسوي اكبر سيرفر انشاء الله 
+و في فقرة نشر سيرفرات كل اسبوع ادخل
+  <https://discord.gg/vF6CG>
+▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬
+**
+[${member}]`);
+
+
+  });
+
+client3.login("NDk0ODk5NDQ0NTAwNjYwMjQ3.Do6PVA.ytKdF8wCq_RA1VGs6MLE0ZxswbM");
